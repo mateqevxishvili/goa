e9c4d239c1fb1219655c30a6c8a926773d4f1775
mateqevxishvili <mateqevxishvili402@gmail.com>
2024-02-14 20:43:32 +0400

    first commit

---
+++ day 1/lesson 1.py
@@ -0,0 +1,6 @@
+name = "Mate"
+surname = "kevkhishvili"
+age = 12
+height = 150.5
+hobby = "programming"
+print(" my name and surname : " + name + " " + surname + " i am " + str(age) + " years old" + " i am " + str(height) + " on height" + " my hobby is " + hobby )
