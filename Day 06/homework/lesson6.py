07555e2e929b5ed26bdf55af16ccf22716c50071
mateqevxishvili <mateqevxishvili402@gmail.com>
2024-03-28 22:55:59 +0400

    commmit

---
+++ Day 06/homework/lesson6.py
@@ -0,0 +1,18 @@
+name = input("please enter your name: ")
+lastname = input("please enter your lastname: ")
+age  = input("please enter your age: ")
+addres = input("please enter your addres: ")
+job = input("please enter your job: ")
+print("გამარჯობა, მე ვარ " + " " + name + " " + lastname + " " + "ვარ" + " " + age + " " + "წლის" + " " + " ჩემი საცხოვრებელი ადგილი არის" + " " + addres + " " + "ჩემის სამუშო არის" + " " + job)
+
+# ვიწყებ მეორე დავალებას 
+num1 = int(input("please enter first number: "))
+num2 = int(input("please enter second number: "))
+num3 = int(input("please enter third number: "))
+num4 = int(input("please enter fourth number: "))
+num5 = int(input("plwase enter fivth number: "))
+
+print(num1 + num2 + num3 + num4 + num5)
+print(num1 - num2 - num3 - num4 - num5)
+print(num1 * num2 * num3 * num4 * num5)
+print(num1 / num2 / num3 / num4 / num5)
